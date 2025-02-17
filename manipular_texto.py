fc666919b07ed51613452076254c4fb121a3c417
joao <joaomatosf240@gmail.com>
2025-02-17 18:26:22 -0300

    treinando comando commit

--- manipular_texto.py
+++ manipular_texto.py
@@ -1,4 +1,4 @@
-#Exercício Python 022: Crie um programa que leia o nome completo de uma pessoa e mostre: 
+#Exercício Python 022 a 27: Crie um programa que leia o nome completo de uma pessoa e mostre: 
 #O nome com todas as letras maiúsculas e minúsculas.
 #Quantas letras ao todo (sem considerar espaços).
 #Quantas letras tem o primeiro nome.
@@ -44,7 +44,8 @@ print(f"E a ultima na {nome.rfind('a')+1}")
 
 #Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 
+'''
 nome = str(input("Qual é o seu nome completo ?")).title().strip().split()
-
 print(f"Seu primeiro nome é {nome[0]}")
 print(f"E o seu ultimo nome é {nome[-1]}")
+'''
