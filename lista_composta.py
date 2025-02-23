004a98672f6c60a4d78cea222bfadc7e6ace321d
joao <joaomatosf240@gmail.com>
2025-02-22 23:41:55 -0300

    Listas compostas

--- lista_composta.py
+++ lista_composta.py
@@ -118,6 +118,7 @@ print(f"E o maior valor da segunda linha é {maior}")'''
 
 #Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
 # O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+'''
 from random import randint
 matriz=[[0],[0],[0],[0],[0],[0]]
 verificar = []
@@ -134,4 +135,43 @@ for c in range(1,jogos+1):
         verificar.append(valor)
     matriz.sort()
     print(f"Seu {c}o jogo esta aqui: {matriz}")
-    verificar.clear()
+    verificar.clear()'''
+
+#Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
+#No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente
+
+boletim = []
+notas = []
+while True:
+    notas.append(str(input("Nome: ")))
+    notas.append(float(input("1o Nota: ")))
+    notas.append(float(input("2o Nota: ")))
+    boletim.append(notas[:])
+    notas.clear()
+    pergunta = str(input("Quer continuar ?")).lower().strip()[0]
+    if pergunta == 's' :
+        continue
+    elif pergunta not in 'n':
+        if (str(input("Apenas sim ou nao, vc que continuar ?")).strip().lower()[0]) == 's':
+            continue
+        break
+    break
+print(boletim)
+print("O boletim registrado é: ")
+for p,nota in enumerate(boletim):
+    media = (nota[1] + nota[2]) / 2
+    print(f"{p}o - O aluno {nota[0]} tem na média: {media}")
+
+
+while True:
+    indice = int(input("Se quiser ver as notas individuais de um aluno, pressione o indice dele (para sair digite -8): "))
+    if indice > -1 and indice < len(boletim):
+        for pos,n in enumerate(boletim):
+            if pos == indice:
+                print(f"Notas: {n[1:]} de {n[0]}")
+                print('-='*15)
+    elif indice == -8:
+        break
+    else:
+        print("Digite uma resposta valida")
+        print('-='*15)
